5f95b1c063a85439a17213c8214732a9ea2b2a1f
facundo <sainifacunddo@gmail.com>
2022-12-21 20:34:11 -0300

    agregada validacion de campos

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const { check } = require("express-validator");
 const {
   createUser,
   editUser,
@@ -10,7 +11,16 @@ const { validateFields } = require("../middlewares/fieldValidator");
 
 const router = Router();
 
-router.post("/", [validateFields], createUser);
+router.post(
+  "/",
+  [
+    check("name", "El nombre es obligatorio").notEmpty(),
+    check("surname", "El apellido es obligatorio").notEmpty(),
+    check("email", "El email es obligatorio").isEmail(),
+    validateFields,
+  ],
+  createUser
+);
 
 router.put("/", [validateFields], editUser);
 
