2b6204ee9231e725928228a8398da466a1ff7d60
facundo <sainifacunddo@gmail.com>
2022-12-27 17:22:16 -0300

    agregado modelo location

---
+++ models/location.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+
+const LocationSchema = Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  coordinates: {
+    type: [Number],
+    required: true,
+  },
+  visits: {
+    type: Number,
+  },
+  fishes: {
+    type: [String],
+  },
+  description: {
+    type: String,
+  },
+  places: {
+    type: [String],
+  },
+  active: {
+    type: Boolean,
+  },
+});
+
+LocationSchema.method("toJSON", function () {
+  const { __v, _id, ...object } = this.toObject();
+  object.id = _id;
+  return object;
+});
+
+module.exports = model("Location", LocationSchema);
